ce962ec1c02ccec9fbae598900b18782400818f1
Brian Norton <nortonbe@gmail.com>
2018-05-22 21:46:44 -0400

    first commit

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.sprite import Group
+
+import game_functions as gf
+from settings import Settings
+from ship import Ship
+
+
+def run_game():
+    """Run the game"""
+    # Initialize game and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game.
+    while True:
+        # Watch for keyboard and mouse events.
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+run_game()
